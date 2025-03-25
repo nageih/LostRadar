ec6d47b60d8af115e9b6da3dfbdc1e9ed9c1e526
McJty <mcjty1@gmail.com>
2025-03-25 07:18:47 +0100

    Small changes

--- src/main/java/mcjty/lostradar/radar/RadarItem.java
+++ src/main/java/mcjty/lostradar/radar/RadarItem.java
@@ -1,11 +1,26 @@
 package mcjty.lostradar.radar;
 
 import mcjty.lostradar.LostRadar;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nonnull;
 
 public class RadarItem extends Item {
 
     public RadarItem() {
         super(LostRadar.setup.defaultProperties().stacksTo(1).defaultDurability(1));
     }
+
+    @Override
+    @Nonnull
+    public InteractionResultHolder<ItemStack> use(@Nonnull Level world, @Nonnull Player player, @Nonnull InteractionHand hand) {
+        GuiRadar.open();
+        return super.use(world, player, hand);
+    }
+
 }
